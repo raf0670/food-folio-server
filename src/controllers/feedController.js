a4a6240601a548f19b7917f05fae212006b4ca70
Rhk32 <rahik3227@gmail.com>
2026-09-24 02:06:24 +0600

    searchbar done

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -2,27 +2,24 @@ const feedService = require('../services/feedService');
 
 const getFeedData = async (req, res) => {
     try {
-        const { lat, lng, city, country, radius } = req.query;
+        const { lat, lng, city, country, radius, search } = req.query;
         const rad = (parseInt(radius) || 10) * 1000;
 
         let reviews = [];
 
         // if location access given
         if (lat && lng) {
-            reviews = await feedService.getReviewsByRadius(lat, lng, rad);
+            reviews = await feedService.getReviewsByRadius(lat, lng, rad, search);
         } 
         // if no location given
         else if (city) {
-            reviews = await feedService.getReviewsByCity(city, country);
+            reviews = await feedService.getReviewsByCity(city, search);
         } 
         else {
             return res.status(400).json({ message: "Location parameters are missing!" });
         }
 
-        return res.status(200).json({
-            success: true,
-            data: reviews
-        });
+        return res.status(200).json(reviews);
 
     } catch (error) {
         console.error("Feed Controller Error:", error);
